8457a15166c54912cb1a2552c2b053361c549056
C:/Program Files/Git/DUONGVU <duonghutech26@gmail.com>
2023-03-28 18:58:33 +0700

    login admin

---
+++ VuDaiDuong_8627_DoAnCoSo/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace VuDaiDuong_8627_DoAnCoSo.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Index", Controller = "Admin", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ VuDaiDuong_8627_DoAnCoSo/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VuDaiDuong_8627_DoAnCoSo.Models;
+
+namespace VuDaiDuong_8627_DoAnCoSo.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        DACSEntities db = new DACSEntities();
+        // GET: Admin/Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+       
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var data = db.Users.Where(n => n.UserName.Equals(username) && n.PassWord.Equals(password));
+                if (data.Count() > 0)
+                {
+                    Session["name"] = data.FirstOrDefault().FullName;
+                    Session["role"] = data.FirstOrDefault().Role;
+                    Session["id"] = data.FirstOrDefault().IdUser;
+                    if (Session["role"] == null)
+                    {
+                        return Redirect("~");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+                else
+                {
+                    //ViewBag.error = "Login Fail";
+                    return RedirectToAction("Login");
+                }
+            }
+            return View();
+        }
+    }
+}
