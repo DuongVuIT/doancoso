052aee4b6614799140535a78fb4ac906041d2d2c
C:/Program Files/Git/DUONGVU <duonghutech26@gmail.com>
2023-04-08 20:40:56 +0700

    sửa lại toàn bộ giao diện

--- VuDaiDuong_8627_DoAnCoSo/Controllers/HomeController.cs
+++ VuDaiDuong_8627_DoAnCoSo/Controllers/HomeController.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
+using System.Web.UI;
+using VuDaiDuong_8627_DoAnCoSo.Models;
 
 namespace VuDaiDuong_8627_DoAnCoSo.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        DACSEntities db = new DACSEntities();
+        public ActionResult Index(int page = 1, int pagesize = 3)
         {
-            return View();
+            return View(db.Products.ToList().ToPagedList(page, pagesize));
         }
+        public ActionResult DetailPro(int id)
+        {
+            Product pro = db.Products.SingleOrDefault(n => n.IdProduct == id);
+            return View(pro);
 
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
